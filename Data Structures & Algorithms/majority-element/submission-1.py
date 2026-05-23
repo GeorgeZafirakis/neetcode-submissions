990768f62d481bafb3353efb6de989ffb4286a53
GeorgeZafirakis <georgezafirakis@icloud.com>
2026-05-23 18:54:07 +0300

    Add: majority-element - submission-1

---
+++ Data Structures & Algorithms/majority-element/submission-1.py
@@ -0,0 +1,18 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        
+        myMap = defaultdict()
+        for num in nums:
+            if num in myMap:
+                myMap[num] += 1
+            else:
+                myMap[num] = 1
+        
+        count = 0
+        for t in myMap:
+            count = max(count, myMap[t])
+
+        for res in myMap:
+            if myMap[res] == count:
+                return res
+        return -1
